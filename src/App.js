885a24694a0103887839cc16d87622a135df069d
ConsoleDotLog-const <c-gusto@hotmail.com>
2021-01-09 16:49:31 +1300

    upload/select image js file created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Title from './components/Title';
+import UploadFrom from './components/UploadFrom'
 
 function App() {
   return (
     <div className="App">
       <Title/>
+      <UploadFrom/>
     </div>
   );
 }
