0d641fd9d8f2f5989fff7e8695608f99c8fd17eb
ConsoleDotLog-const <c-gusto@hotmail.com>
2021-01-11 09:15:38 +1300

    css for form/label & connected to firestore

--- src/components/UploadFrom.js
+++ src/components/UploadFrom.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import ProgressBar from './ProgressBar'
 
 const UploadFrom = () => {
 
@@ -22,10 +23,15 @@ const UploadFrom = () => {
 
     return(
         <form>
+            <label>
             <input type="file" onChange={changeHandler} />
+            <span>+</span>
+            </label>
+        
             <div className="output">
                 { error && <div className="error">{ error}</div>}
                 { file && <div>{ file.name}</div>}
+                { file && <ProgressBar file={file} setFile={setFile}/>}
             </div>
         </form>
         
