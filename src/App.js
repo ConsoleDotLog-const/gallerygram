261fc4c9a63610309c2a640d0dbf1f9023f4fe9f
ConsoleDotLog-const <c-gusto@hotmail.com>
2021-01-11 09:48:14 +1300

    firestore connected and shows images on page IRT

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Title from './components/Title';
 import UploadFrom from './components/UploadFrom'
+import ImageGrid from './components/ImageGrid'
 
 function App() {
   return (
     <div className="App">
       <Title/>
       <UploadFrom/>
+      <ImageGrid/>
     </div>
   );
 }
